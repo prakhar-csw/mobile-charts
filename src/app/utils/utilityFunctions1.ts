702649738888ed3365ef6804ccd10386fc4fff9d
prakhar-csw <prakhar.s@coinswitch.co>
2024-03-04 15:27:52 +0530

    TypeScript code added

---
+++ src/app/utils/utilityFunctions1.ts
@@ -0,0 +1,67 @@
+import { ChartingLibraryWidgetOptions, LanguageCode, ResolutionString, widget } from "../../../public/charting_library";
+
+
+import TradingView from '../../../public/charting_library/charting_library';
+import {UDFCompatibleDatafeed} from '../../../public/datafeeds/udf';
+
+export const getParameterByName = (name:string):string => {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    const regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+    const results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+  };
+
+export const initOnReady = (datafeedUrl:string):void => {
+    // var datafeedUrl = "https://demo-feed-data.tradingview.com";
+    var customDataUrl = getParameterByName('dataUrl');
+    if (customDataUrl !== "") {
+        datafeedUrl = customDataUrl.startsWith('https://') ? customDataUrl : `https://${customDataUrl}`;
+    }
+
+    const widgetOptions: ChartingLibraryWidgetOptions = {
+        fullscreen: false,
+        symbol: 'AAPL',
+        interval: '1D' as ResolutionString,
+        container: "tv_chart_container",
+        autosize:true,
+        datafeed: new (window as any).Datafeeds.UDFCompatibleDatafeed(
+            "https://demo_feed.tradingview.com",
+            undefined,
+            {
+                maxResponseLength: 1000,
+                expectedOrder: "latestFirst",
+            }
+        ),
+        library_path: "charting_library/",
+        locale:<any> getParameterByName('lang') || "en",
+    
+        disabled_features: ["use_localstorage_for_settings"],
+        enabled_features: ["study_templates"],
+        charts_storage_url: 'https://saveload.tradingview.com',
+        charts_storage_api_version: "1.1",
+        client_id: 'tradingview.com',
+        user_id: 'public_user_id',
+        theme:<any> getParameterByName('theme'),
+    };
+
+    const tvWidget = new widget(widgetOptions);
+    tvWidget.onChartReady(() => {
+        tvWidget.headerReady().then(() => {
+            const button = tvWidget.createButton();
+            button.setAttribute("title", "Click to show a notification popup");
+            button.classList.add("apply-common-tooltip");
+            button.addEventListener("click", () =>
+                tvWidget.showNoticeDialog({
+                    title: "Notification",
+                    body: "TradingView Charting Library API works correctly",
+                    callback: () => {
+                        console.log("Noticed!");
+                    },
+                })
+            );
+
+            button.innerHTML = "Check API";
+        });
+    });
+    window.frames[0].focus();
+};
